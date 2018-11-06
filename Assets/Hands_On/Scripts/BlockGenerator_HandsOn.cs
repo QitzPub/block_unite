9025e997fd5bbdf3195a735baa2df725d8d7dba1
QitzPub <ikauka456@gmail.com>
2018-11-06 17:53:23 +0900

    ハンズオン用アセットを追加

---
+++ Assets/Hands_On/Scripts/BlockGenerator_HandsOn.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BlockGenerator_HandsOn : MonoBehaviour
+{
+    //SerializeFieldが必要な変数を特定し、記述して設定する
+    GameObject Block;
+    GameObject newBlock;
+    Transform BlockContainer;
+    int InitialSpawnNumber;
+    int FeverSpawnNumber;
+
+    // ゲーム開始時にこの中の関数が実行される
+    void Start()
+    {
+        InitialSpawn();
+    }
+    //位置(x,y,z)を取得する関数
+    Vector3 SpawnPosition(){
+        Vector3 position;
+        //x,y座標を適切に再設定する
+        position.x = 5000000000000000;
+        position.y = 5000000000000000;
+        position.z = 0;
+        return position;
+    }
+    //色情報を取得する関数
+    Color BlockColor(){
+        //MasterDataのColorListの中から１色を返す
+        return MasterData.ColorList.OrderBy(i => Guid.NewGuid()).First();
+    }
+    //ブロック１つを生成する関数
+    public void SpawnOnce(){
+        //ブロックプレハブ(Block)をインスタンス化し、クローン(newBlock)とする
+        //newBlock = 
+
+        //BlockContainerを親オブジェクトに設定する
+        //newBlock.transform.
+
+        //SpawnPosition関数を利用し、生成位置を設定する
+        //newBlock.transform.
+
+        //BlockColor関数を利用して、Rendererの色を変更する
+        //newBlock.
+    }
+    void InitialSpawn(){
+        //InitialSpawnNumberの回数だけ、ブロックを生成する（SpawnOnce関数を実行する）
+        //for ()
+        //{
+
+        //}
+    }
+    public void SpawnManyTimes(){
+        //FeverSpawnNumberの回数だけ、ブロックを生成する（SpawnOnce関数を実行する）
+        //for ()
+        //{
+
+        //}
+    }
+}
---
+++ Assets/Hands_On/Scripts/BlockUniter_HandsOn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockUniter_HandsOn : MonoBehaviour
+{
+    //コライダー内にオブジェクトが入ると実行される。入った相手のオブジェクトはcolsとする
+    private void OnCollisionEnter2D(Collision2D cols)
+    {
+        //このスクリプトがアタッチされているオブジェクト(newBlock)の色を取得
+        Color selfColor = this.transform.GetComponent<Renderer>().material.color;
+        //Collider内に入った相(cols)の色を取得
+        Color oppoColor = cols.transform.GetComponent<Renderer>().material.color;
+
+        //生成時に自動付与されたIDを取得
+        int selfID = this.gameObject.GetInstanceID();
+        int oppoID = cols.gameObject.GetInstanceID();
+
+        //同色　かつ　IDが大きいときに合体させる
+        if (selfColor == oppoColor && selfID >= oppoID)
+        {
+            //２つのオブジェクトの中点を計算し、移動する
+            //this.transform.position = 
+
+            //Rigidbody2Dコンポーネントを取得する
+            Rigidbody2D selfrb = this.GetComponent<Rigidbody2D>();
+            Rigidbody2D opporb = cols.transform.GetComponent<Rigidbody2D>();
+
+            //相手の質量を、自分の質量に足す(selfrb、opporbを利用できる)
+            //
+
+            //衝突"相手"を消去する
+            //Destroy();
+
+        }
+    }
+}
